13cfe5dd54ef0cc196834562fa47fcdb28d904c9
klauener <kilian.lauener@swisscom.com>
2025-04-14 17:12:57 +0200

    Bad example

--- src/main/java/org/example/ECDSAExample.java
+++ src/main/java/org/example/ECDSAExample.java
@@ -1,14 +1,29 @@
 package org.example;
-import java.security.*;
-import java.io.*;
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.SecureRandom;
+import java.util.Arrays;
 
 public class ECDSAExample {
 
     public static void main(String[] args) throws Exception {
-      KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-      kpg.initialize(2048);
-      KeyPair kp = kpg.generateKeyPair();
-      Files.write(new File("pub.der").toPath(), kp.getPublic().getEncoded());
-      Files.write(new File("priv.der").toPath(), kp.getPrivate().getEncoded());
+      SecretKey key = generateKey();
+      String data = "Hello";
+      String iv = "bad iv 123456789";
+      
+      byte[] ct = encrypt(key, data.getBytes(), iv.getBytes());
+      System.out.println(Arrays.toString(ct));
+   }
+
+    public static SecretKey generateKey() throws Exception {
+        KeyGenerator kg = KeyGenerator.getInstance("AES");
+        kg.init(256);
+        return kg.generateKey();
+    }
+
+    public static byte[] encrypt(SecretKey key, byte[] data, byte[] iv) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv));
+        return cipher.doFinal(data);
     }
 }
